b507d8f59bfae77ccce7c8720449acec5c0cd750
dadysherwin1 <86547852+dadysherwin1@users.noreply.github.com>
2023-09-15 11:54:07 +1000

    faaack

---
+++ Week5bro.py
@@ -0,0 +1,68 @@
+import os
+import time
+from os import listdir
+from os.path import isfile, join
+import re
+import maya.cmds as cmds
+path = 'C:/Users/24562353/Desktop/Week5/'
+#cmds.AbcImport(h=True)
+
+def getVersion(prop):
+    count = 0
+    files = [f for f in listdir(path + prop) if isfile(join(path + prop, f))]
+    for fileName in files:
+        if re.search(prop + "_...\.abc" , fileName):
+            count += 1
+    return count
+    
+def versionToString(version):
+    return "%03d" % (version)
+
+def saveProp(prop):
+    # make directory if needed
+    if not os.path.exists(path + prop):
+        os.makedirs(path + prop)
+    
+    command = "-frameRange 1 600 -step 1 -sl -file " + path + prop + "/" + prop + "_" + versionToString(getVersion(prop)+1) + ".abc"
+    cmds.AbcExport(j = command)
+    
+def onExportSelected(*args):
+    props = cmds.ls(selection=True)
+    for prop in props:
+        
+        saveProp(prop)
+
+def onExportAll(*args):
+    props = cmds.ls(geometry=True)
+    for prop in props:
+        cmds.select(prop)
+        saveProp(prop)
+        cmds.select(prop, toggle=False) 
+    
+def onImport(*args):
+    cmds.file(f=True, new=True)
+    folders = [f for f in listdir(path) if not isfile(join(path, f))]
+    for prop in folders:
+        command = path + prop + "/" + prop + "_" + versionToString(getVersion(prop)) + ".abc"
+        print(command)
+        cmds.AbcImport(command, mode="import")
+
+cmds.window(widthHeight=(10,10), resizeToFitChildren=True)
+cmds.columnLayout(columnAlign="left")
+
+cmds.separator(h=10, style="none")
+
+cmds.text(label="Save props to file")
+cmds.separator(h=10, style="none")
+cmds.rowLayout(numberOfColumns=2)
+cmds.button(label="Export Selected", command=onExportSelected, width=200)
+cmds.button(label="Export All", command=onExportAll, width=200)
+cmds.setParent("..")
+
+cmds.separator(h=30, style="none")
+
+cmds.text(label="Creates a new scene with all set props")
+cmds.separator(h=10, style="none")
+cmds.button(label="Import", command=onImport, width=400)
+
+cmds.showWindow()
