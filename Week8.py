043c79fc2154f44d202e95675595fc4ea3cecfb3
dadysherwin1 <86547852+dadysherwin1@users.noreply.github.com>
2023-10-13 11:56:13 +1100

    Lighting tool

--- Week8.py
+++ Week8.py
@@ -5,12 +5,17 @@ import maya.cmds as cmds
 sequenceName = ""
 shotName = ""
 
-def exportSequenceLighting():
-    cmds.file(path + sequence + "/sequence_lighting.mb", force=True, typ="mayaBinary", exportSelected=True)
-    
-def exportSequenceLighting():
-    cmds.file(path + sequence + "/" + shotName + "/" + ".mb", force=True, typ="mayaBinary", exportSelected=True)
+def getTextFields():
+    sequenceName = cmds.textField("sequence", q=True, text=True)
+    shotName = cmds.textField("shot", q=True, text=True)
 
+def exportSequenceLighting(buttonInfo):
+    getTextFields()
+    cmds.file(path + sequenceName + "/sequence_lighting.mb", force=True, typ="mayaBinary", exportSelected=True)
+    
+def exportShotLighting(buttonInfo):
+    getTextFields()
+    cmds.file(path + sequenceName + "/" + shotName + ".mb", force=True, typ="mayaBinary", exportSelected=True)
 
 cmds.window(widthHeight=(10,10), resizeToFitChildren=True)
 cmds.columnLayout(columnAlign="left")
@@ -18,14 +23,15 @@ cmds.columnLayout(columnAlign="left")
 # sequence and shot text fields
 cmds.separator(h=10, style="none")
 cmds.rowLayout(numberOfColumns=2)
-cmds.text(label="Sequence name (if applicable): ", width=200)
-sequenceName = cmds.textField(width=200)
+cmds.text(label="Sequence name: ", width=200)
+cmds.textField("sequence",width=200)
 cmds.setParent("..")
 cmds.rowLayout(numberOfColumns=2)
 cmds.text(label="Shot name (if applicable): ", width=200)
-shotName = cmds.textField(width=200)
+cmds.textField("shot",width=200)
 
 # export 
+cmds.setParent("..")
 cmds.separator(h=30, style="none")
 cmds.text(label="Export selected lights")
 cmds.separator(h=10, style="none")
